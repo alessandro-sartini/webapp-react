a76570304e5428c64921dfc82b4a7bc8fd36f6c7
alessandro-sartini <ale.sartini.98@gmail.com>
2025-03-17 12:01:30 +0100

    Modify navBar

--- src/components/headerComponents/Header.jsx
+++ src/components/headerComponents/Header.jsx
@@ -3,13 +3,34 @@ import { NavLink } from "react-router-dom";
 export default function Header() {
   return (
     <header>
-      <nav className="navbar navbar-expand navbar-light bg-light">
-        <div className="nav navbar-nav">
-          <NavLink className="nav-item nav-link" to="/">
-            Home
-          </NavLink>
-        </div>
-      </nav>
+      
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+                <NavLink className="nav-link active" aria-current="page" href="#">
+                  Home
+                </NavLink>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
     </header>
   );
 }
+
+        {/* <li className="nav-item">
+          <a className="nav-link" href="#">Features</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Pricing</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li> */}
