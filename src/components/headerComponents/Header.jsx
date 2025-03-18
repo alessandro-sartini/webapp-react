2a80e0d5d135ed498c3d46c97dd02c4726c26d76
alessandro-sartini <ale.sartini.98@gmail.com>
2025-03-18 16:26:39 +0100

    modify: NavBar

--- src/components/headerComponents/Header.jsx
+++ src/components/headerComponents/Header.jsx
@@ -3,29 +3,39 @@ import { Link, NavLink } from "react-router-dom";
 export default function Header() {
   return (
     <header>
-      
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <Link className="navbar-brand" to="/">Navbar</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-                <NavLink className="nav-link active" aria-current="page" to="/">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            ReviewBlog
+          </Link>
+          {/* <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button> */}
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link" aria-current="page" to="/">
                   Home
                 </NavLink>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </header>
   );
 }
 
-        {/* <li className="nav-item">
+{
+  /* <li className="nav-item">
           <a className="nav-link" href="#">Features</a>
         </li>
         <li className="nav-item">
@@ -33,4 +43,5 @@ export default function Header() {
         </li>
         <li className="nav-item">
           <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li> */}
+        </li> */
+}
